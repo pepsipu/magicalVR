98e0e004bdb896f3fde7255573028b22d2701591
Sammy H <pokeso123@hotmail.com>
2020-02-22 23:57:41 -0800

    added rock barrage skill for earth magic

---
+++ Assets/Scripts/Magic/Earth/earthMagic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class earthMagic : MonoBehaviour
+{
+    private float timeHeld = 0f;
+    public GameObject rockBarragePrefab;
+    public Transform rightHand;
+    public Transform leftHand;
+    private GameObject currentRockBarrage;
+    private bool active = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.A))
+        {
+            timeHeld += Time.deltaTime;
+            if (timeHeld >= 1f && !active)
+            {
+                timeHeld = timeHeld % 1f;
+                currentRockBarrage = Instantiate(rockBarragePrefab, rightHand.position, rightHand.rotation);
+                active = true;
+            }
+        }
+        else
+        {
+            timeHeld = 0f;
+            if (active)
+            {
+                active = false;
+                Destroy(currentRockBarrage);
+            }
+        }
+
+    }
+}
